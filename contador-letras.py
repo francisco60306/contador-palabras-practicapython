c299c7b64b6289df9cd5a53d6c8dbe58aea5d65a
franciscorampazzo0@gmail.com <franciscorampazzo0@gmail.com>
2024-08-01 23:05:35 -0300

    practica 3 de curso python contador de palabras con diagramas de flujo y pseudocodigo

---
+++ contador-letras.py
@@ -0,0 +1,10 @@
+# paso 1: solicitar entrada al usuario
+
+palabra_ingresada = input("por favor ingrese la palabra: ")
+
+# paso 2: contar la cantidad de letras
+
+cant_letras = len(palabra_ingresada)
+# paso 3: mostrar resultado
+
+print("la palabra ingresada tiene", cant_letras, "letras.")
